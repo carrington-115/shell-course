84129bc83fe1a609dce14425386b68d091af5224
Mark Carrington <frumarkcarrington@gmail.com>
2024-05-22 09:29:36 +0530

    updated the app

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, data: [] });
+});
+
+app.listen(5000, () => console.log("server is running"));
